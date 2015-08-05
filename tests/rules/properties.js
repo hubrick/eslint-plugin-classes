97c4dbd3b5ef6cd04b03dd2b6ca2d67bf2bbc193
dimofte <dimofte@gmail.com>
2015-08-05 12:00:35 +0200

    Properties on top: rule implementation

--- tests/rules/properties.js
+++ tests/rules/properties.js
@@ -13,35 +13,43 @@ ESLintTester.setDefaultConfig({
 var eslintTester = new ESLintTester(linter);
 
 var valid = [
-  //'class Foo {' +
-  //'  bar = 3; ' +
-  //'}          ',
-  //'class Foo {' +
-  //'  bar = 2;' +
-  //'  baz() {}' +
-  //'}          ',
-  //'class Foo {' +
-  //'  /* comments allowed */' +
-  //'  bar = 2;' +
-  //'  baz() {}' +
-  //'}          ',
-  //'class Foo {' +
-  //'  bar = 2;' +
-  //'  baz   () {} /* <-- note the spaces */' +
-  //'}          ',
-  //'class Foo {' +
-  //'  bar() {}' +
-  //'}          ',
-  //'class Foo {' +
-  //'  static bar = 1;' +
-  //'  baz() {}' +
-  //'}          ',
+
+  'class Foo {' +
+  '  bar = 3; ' +
+  '}          ',
+
+  'class Foo {' +
+  '  bar = 2;' +
+  '  baz() {}' +
+  '}          ',
+
+  'class Foo {' +
+  '  /* comments allowed */' +
+  '  bar = 2;' +
+  '  baz() {}' +
+  '}          ',
+
+  'class Foo {' +
+  '  bar = 2;' +
+  '  baz   () {} /* <-- note the spaces */' +
+  '}          ',
+
+  'class Foo {' +
+  '  bar() {}' +
+  '}          ',
+
+  'class Foo {' +
+  '  static bar = 1;' +
+  '  baz() {}' +
+  '}          ',
+
   'class Foo {' +
   '  static staticProp = 1;' +
   '  prop = 2;' +
   '  method() {' +
-  '    var aSimpleVar = 3;' +
-  '    function aSimpleFunction() { ' +
+  '    var simpleVar = 3;' +
+  '    globalVar = 4;' +
+  '    function simpleFunction() { ' +
   '       var anotherSimpleVar;' +
   '    }' +
   '  }' +
@@ -49,26 +57,29 @@ var valid = [
 ].map(function(code) {
       return {
         code: code,
-        args: [ 1, 'properties-first' ],
+        args: [ 1, 'on-top' ],
         ecmaFeatures: { classes: true }
       };
     });
 
 var invalidClass = [
-  //'class Foo {' +
-  //'  baz() {}' +
-  //'  static bar = 1;' +
-  //'}          ',
-  //'class Foo {' +
-  //'  baz() {}' +
-  //'  bar = 2;' +
-  //'}          '
+
+  'class Foo {' +
+  '  baz() {}' +
+  '  static bar = 1;' +
+  '}          ',
+
+  'class Foo {' +
+  '  alpha() {}' +
+  '  beta = 2;' +
+  '  gamma() {}' +
+  '}          '
 ].map(function(code) {
       return {
         code: code,
-        args: [ 1, 'properties-first' ],
+        args: [ 1, 'on-top' ],
         ecmaFeatures: { classes: true },
-        errors: [{ message: 'properties should be defined in the beginning of the class.' }]
+        errors: [{ message: 'properties should be declared on top.' }]
       };
     });
 
